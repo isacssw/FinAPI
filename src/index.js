664df5c6494c54f7597b67c95168ffb3fb291930
Isac X <isacssw@gmail.com>
2021-03-20 18:58:48 +0000

    creating API with business rules

--- src/index.js
+++ src/index.js
@@ -3,18 +3,70 @@ const {v4: uuidv4} = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
 const customers = [];
+
+// middleware
+
+function verifyIfExistsAccountCPF(req, res, next) {
+    const {cpf} = req.headers;
+
+    const customer = customers.find( customer => customer.cpf === cpf);
+
+    if (!customer) {
+        return res.status(400).json({ error: "Customer not found"});
+    }
+
+    req.customer = customer;
+
+    return next();
+}
 
 app.post("/account", (req, res) => {
     const {cpf, name} = req.body;
-    const id = uuidv4();
+
+    const customerAlredyExists = customers.some((customer)=> customer.cpf === cpf);
+
+    if (customerAlredyExists) {
+        return res.status(400).json({ error: "Customer alredy exists"});
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidv4(),
         statement: []
     });
+
+    return res.status(201).send();
+});
+
+//app.use(verifyIfExistsAccountCPF);
+
+app.get("/statement", verifyIfExistsAccountCPF, (req, res) =>{
+
+    const {customer} = req;
+
+    return res.json(customer.statement);
+
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) =>{
+    const {description, amount} = req.body;
+
+    const {customer} = req;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
 });
 
 app.listen(3333);
