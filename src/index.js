eaf0bc0fe49e68e64763fab181b2a009f155e997
Isac X <isacssw@gmail.com>
2021-03-20 19:57:34 +0000

    creatting an updtade option

--- src/index.js
+++ src/index.js
@@ -124,4 +124,33 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
 
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+
+    const {customer} = req;
+    const {date} = req.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter(statement => statement.created_at.toDateString() === new Date(dateFormat).toDateString())
+
+    return res.json(customer.statement);
+
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (req, res) => {
+    const {name} = req.body;
+    const {customer} = req;
+
+    customer.name = name;
+
+    return res.status(201).send();
+
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (req, res)=>{
+    const {customer} = req;
+
+    return res.json(customer);
+})
+
 app.listen(3333);
